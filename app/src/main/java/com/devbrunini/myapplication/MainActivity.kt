2e480a872ba8ecd83a8740db08bc20309144d7b3
Heitor Brunini <68927093+heitorbrunini@users.noreply.github.com>
2024-05-24 17:17:43 -0300

    viewBinding

--- app/src/main/java/com/devbrunini/myapplication/MainActivity.kt
+++ app/src/main/java/com/devbrunini/myapplication/MainActivity.kt
@@ -5,13 +5,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.devbrunini.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    //variável é lateinit, não pode ser instanciada ainda: tela ainda não foi chamada em onCreate
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -19,4 +24,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun calculate(){
+
+    }
 }
