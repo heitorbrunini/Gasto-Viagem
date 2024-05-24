f3a689e88c5fd205c3a2b460f366b61b3f4c352d
Heitor Brunini <68927093+heitorbrunini@users.noreply.github.com>
2024-05-24 17:48:01 -0300

    View OnClickListener

--- app/src/main/java/com/devbrunini/myapplication/MainActivity.kt
+++ app/src/main/java/com/devbrunini/myapplication/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.devbrunini.myapplication
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.devbrunini.myapplication.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     //variável é lateinit, não pode ser instanciada ainda: tela ainda não foi chamada em onCreate
     private lateinit var binding : ActivityMainBinding
@@ -22,9 +24,19 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.buttonCalculate.setOnClickListener(this)
+
     }
 
     private fun calculate(){
+        Toast.makeText(this, "Click!", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.button_calculate){
+            calculate()
+        }
 
     }
 }
